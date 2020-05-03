7ae241b6a526cd2932fc2c416a049ba32390ca9c
Natalia Portillo <claunia@claunia.com>
2020-05-03 01:34:13 +0100

    Add NetWare target.

---
+++ setter/src/netware.c
@@ -0,0 +1,110 @@
+/****************************************************************************
+Aaru Data Preservation Suite
+-----------------------------------------------------------------------------
+
+Filename       : dos.c
+Author(s)      : Natalia Portillo
+
+Component      : fstester.setter
+
+--[ Description ] -----------------------------------------------------------
+
+Contains DOS code
+
+--[ License ] ---------------------------------------------------------------
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as
+     published by the Free Software Foundation, either version 3 of the
+     License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warraty of
+     MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-----------------------------------------------------------------------------
+Copyright (C) 2011-2020 Natalia Portillo
+*****************************************************************************/
+
+#if defined(__NETWARE_386__)
+
+#include "netware.h"
+
+#include "consts.h"
+#include "defs.h"
+
+void GetOsInfo()
+{
+    // TODO: Implement
+}
+
+void GetVolumeInfo(const char *path, size_t *clusterSize)
+{
+    // TODO: Implement
+    *clusterSize = 0;
+}
+
+void FileAttributes(const char *path)
+{
+    // TODO: Implement
+}
+
+void FilePermissions(const char *path)
+{
+    // TODO: Implement
+}
+
+void ExtendedAttributes(const char *path)
+{
+    // TODO: Implement
+}
+
+void ResourceFork(const char *path)
+{
+    // TODO: Implement
+}
+
+void Filenames(const char *path)
+{
+    // TODO: Implement
+}
+
+void Timestamps(const char *path)
+{
+    // TODO: Implement
+}
+
+void DirectoryDepth(const char *path)
+{
+    // TODO: Implement
+}
+
+void Fragmentation(const char *path, size_t clusterSize)
+{
+    // TODO: Implement
+}
+
+void Sparse(const char *path)
+{
+    // TODO: Implement
+}
+
+void Links(const char *path)
+{
+    // TODO: Implement
+}
+
+void MillionFiles(const char *path)
+{
+    // TODO: Implement
+}
+
+void DeleteFiles(const char *path)
+{
+    // TODO: Implement
+}
+
+#endif
---
+++ setter/src/netware.h
@@ -0,0 +1,39 @@
+/****************************************************************************
+Aaru Data Preservation Suite
+-----------------------------------------------------------------------------
+
+Filename       : dos.h
+Author(s)      : Natalia Portillo
+
+Component      : fstester.setter
+
+--[ Description ] -----------------------------------------------------------
+
+Contains DOS definitions
+
+--[ License ] ---------------------------------------------------------------
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as
+     published by the Free Software Foundation, either version 3 of the
+     License, or (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warraty of
+     MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-----------------------------------------------------------------------------
+Copyright (C) 2011-2020 Natalia Portillo
+*****************************************************************************/
+
+#if defined(__NETWARE_386__)
+
+#ifndef AARU_FSTESTER_SETTER_NETWARE_H
+#define AARU_FSTESTER_SETTER_NETWARE_H
+
+#endif
+
+#endif
