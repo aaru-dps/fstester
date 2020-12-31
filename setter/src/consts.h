9ebfb0d64401ba0d3ceddae1789ed17804eb3bd9
Natalia Portillo <claunia@claunia.com>
2020-12-31 23:13:50 +0000

    Update copyright year.

--- setter/src/consts.h
+++ setter/src/consts.h
@@ -26,7 +26,7 @@ Constants
      along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -----------------------------------------------------------------------------
-Copyright (C) 2011-2020 Natalia Portillo
+Copyright (C) 2011-2021 Natalia Portillo
 *****************************************************************************/
 
 #ifndef AARU_FSTESTER_SETTER_CONSTS_H
