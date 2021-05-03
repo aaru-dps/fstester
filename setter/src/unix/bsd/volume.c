41a2174acbf81551f565058462406a70eae6e101
Natalia Portillo <claunia@claunia.com>
2021-05-03 12:55:11 +0100

    Fix NetBSD flags definitions.

--- setter/src/unix/bsd/volume.c
+++ setter/src/unix/bsd/volume.c
@@ -47,172 +47,172 @@ void NetBsdPrintStatvfsFlags(unsigned long flags)
 {
     log_write("\tFlags:\n");
 
-    if(flags & MNT_RDONLY)
+    if(flags & ST_RDONLY)
     {
         log_write("\t\tVolume is read-only.\n");
-        flags -= MNT_RDONLY;
+        flags -= ST_RDONLY;
     }
 
-    if(flags & MNT_NOSUID)
+    if(flags & ST_NOSUID)
     {
         log_write("\t\tVolume ignores suid and sgid bits.\n");
-        flags -= MNT_NOSUID;
+        flags -= ST_NOSUID;
     }
 
-    if(flags & MNT_NODEV)
+    if(flags & ST_NODEV)
     {
         log_write("\t\tVolume disallows access to device special files.\n");
-        flags -= MNT_NODEV;
+        flags -= ST_NODEV;
     }
 
-    if(flags & MNT_NOEXEC)
+    if(flags & ST_NOEXEC)
     {
         log_write("\t\tVolume disallows program execution.\n");
-        flags -= MNT_NOEXEC;
+        flags -= ST_NOEXEC;
     }
 
-    if(flags & MNT_SYNCHRONOUS)
+    if(flags & ST_SYNCHRONOUS)
     {
         log_write("\t\tVolume writes are synced at once.\n");
-        flags -= MNT_SYNCHRONOUS;
+        flags -= ST_SYNCHRONOUS;
     }
 
-    if(flags & MNT_NOATIME)
+    if(flags & ST_NOATIME)
     {
         log_write("\t\tVolume does not update access times.\n");
-        flags -= MNT_NOATIME;
+        flags -= ST_NOATIME;
     }
 
-    if(flags & MNT_UNION)
+    if(flags & ST_UNION)
     {
         log_write("\t\tVolume is a union.\n");
-        flags -= MNT_UNION;
+        flags -= ST_UNION;
     }
 
-    if(flags & MNT_ASYNC)
+    if(flags & ST_ASYNC)
     {
         log_write("\t\tVolume is written asynchronously.\n");
-        flags -= MNT_ASYNC;
+        flags -= ST_ASYNC;
     }
 
-    if(flags & MNT_NOCOREDUMP)
+    if(flags & ST_NOCOREDUMP)
     {
         log_write("\t\tVolume shall not have core dumps written.\n");
-        flags -= MNT_NOCOREDUMP;
+        flags -= ST_NOCOREDUMP;
     }
 
-    if(flags & MNT_RELATIME)
+    if(flags & ST_RELATIME)
     {
         log_write("\t\tVolume only updates access time on changes.\n");
-        flags -= MNT_RELATIME;
+        flags -= ST_RELATIME;
     }
 
-    if(flags & MNT_IGNORE)
+    if(flags & ST_IGNORE)
     {
         log_write("\t\tVolume does not appear in df.\n");
-        flags -= MNT_IGNORE;
+        flags -= ST_IGNORE;
     }
 
-    if(flags & MNT_SYMPERM)
+    if(flags & ST_SYMPERM)
     {
         log_write("\t\tVolume recognizes symbolic link permissions.\n");
-        flags -= MNT_SYMPERM;
+        flags -= ST_SYMPERM;
     }
 
-    if(flags & MNT_NODEVMTIME)
+    if(flags & ST_NODEVMTIME)
     {
         log_write("\t\tVolume does not update modification times for devices.\n");
-        flags -= MNT_NODEVMTIME;
+        flags -= ST_NODEVMTIME;
     }
 
-    if(flags & MNT_SOFTDEP)
+    if(flags & ST_SOFTDEP)
     {
         log_write("\t\tVolume uses soft dependencies.\n");
-        flags -= MNT_SOFTDEP;
+        flags -= ST_SOFTDEP;
     }
 
-    if(flags & MNT_LOG)
+    if(flags & ST_LOG)
     {
         log_write("\t\tVolume uses logging.\n");
-        flags -= MNT_LOG;
+        flags -= ST_LOG;
     }
 
-    if(flags & MNT_EXTATTR)
+    if(flags & ST_EXTATTR)
     {
         log_write("\t\tVolume has extended attributes enabled.\n");
-        flags -= MNT_EXTATTR;
+        flags -= ST_EXTATTR;
     }
 
-    if(flags & MNT_EXRDONLY)
+    if(flags & ST_EXRDONLY)
     {
         log_write("\t\tVolume is exported read-only.\n");
-        flags -= MNT_EXRDONLY;
+        flags -= ST_EXRDONLY;
     }
 
-    if(flags & MNT_EXPORTED)
+    if(flags & ST_EXPORTED)
     {
         log_write("\t\tVolume is exported.\n");
-        flags -= MNT_EXPORTED;
+        flags -= ST_EXPORTED;
     }
 
-    if(flags & MNT_DEFEXPORTED)
+    if(flags & ST_DEFEXPORTED)
     {
         log_write("\t\tVolume is exported to the world.\n");
-        flags -= MNT_DEFEXPORTED;
+        flags -= ST_DEFEXPORTED;
     }
 
-    if(flags & MNT_EXPORTANON)
+    if(flags & ST_EXPORTANON)
     {
         log_write("\t\tVolume uses anon id.\n");
-        flags -= MNT_EXPORTANON;
+        flags -= ST_EXPORTANON;
     }
 
-    if(flags & MNT_EXKERB)
+    if(flags & ST_EXKERB)
     {
         log_write("\t\tVolume is exported with Kerberos ID.\n");
-        flags -= MNT_EXKERB;
+        flags -= ST_EXKERB;
     }
 
-    if(flags & MNT_EXNORESPORT)
+    if(flags & ST_EXNORESPORT)
     {
         log_write("\t\tVolume export does not enforce reserved ports.\n");
-        flags -= MNT_EXNORESPORT;
+        flags -= ST_EXNORESPORT;
     }
 
-    if(flags & MNT_EXPUBLIC)
+    if(flags & ST_EXPUBLIC)
     {
         log_write("\t\tVolume is exported publicly.\n");
-        flags -= MNT_EXPUBLIC;
+        flags -= ST_EXPUBLIC;
     }
 
-    if(flags & MNT_LOCAL)
+    if(flags & ST_LOCAL)
     {
         log_write("\t\tVolume is local.\n");
-        flags -= MNT_LOCAL;
+        flags -= ST_LOCAL;
     }
 
-    if(flags & MNT_QUOTA)
+    if(flags & ST_QUOTA)
     {
         log_write("\t\tVolume has quotas enabled.\n");
-        flags -= MNT_QUOTA;
+        flags -= ST_QUOTA;
     }
 
-    if(flags & MNT_ROOTFS)
+    if(flags & ST_ROOTFS)
     {
         log_write("\t\tVolume is the root filesystem.\n");
-        flags -= MNT_ROOTFS;
+        flags -= ST_ROOTFS;
     }
 
-    if(flags & MNT_DISCARD)
+    if(flags & ST_DISCARD)
     {
         log_write("\t\tVolume is TRIMmed/DISCARDed.\n");
-        flags -= MNT_DISCARD;
+        flags -= ST_DISCARD;
     }
 
-    if(flags & MNT_AUTOMOUNTED)
+    if(flags & ST_AUTOMOUNTED)
     {
         log_write("\t\tVolume is automounted.\n");
-        flags -= MNT_AUTOMOUNTED;
+        flags -= ST_AUTOMOUNTED;
     }
 
     if(flags) { log_write("\t\tRemaining flags: 0x%08lX\n", flags); }
